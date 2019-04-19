eda09dc5af85be166e69ff53524ae050f128966c
pranayamishra <pmishra@tradon.biz>
2019-04-19 22:21:32 +0200

    registration 1

---
+++ src/app/registration.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { User } from './user';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistrationService {
+
+  constructor() { }
+
+  public register(userInfo: User) {
+
+  }
+}
