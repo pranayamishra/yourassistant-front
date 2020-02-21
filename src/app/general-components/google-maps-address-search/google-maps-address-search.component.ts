df8e598f5313c5bf2fefa0d5a8bfe9cdeb1bbea9
pranayamishra <pmishra@tradon.biz>
2020-02-21 19:19:22 +0100

    first run of the maps

--- src/app/general-components/google-maps-address-search/google-maps-address-search.component.ts
+++ src/app/general-components/google-maps-address-search/google-maps-address-search.component.ts
@@ -33,11 +33,11 @@ interface Location {
 export class GoogleMapsAddressSearchComponent implements OnInit {
 	geocoder: any;
 	public location: Location = {
-		lat: 51.678418,
-		lng: 7.809007,
+		lat: 171.678418,
+		lng: 1.809007,
 		marker: {
-			lat: 51.678418,
-			lng: 7.809007,
+			lat: 181.678418,
+			lng: 17.809007,
 			draggable: true
 		},
 		zoom: 5
@@ -57,75 +57,79 @@ export class GoogleMapsAddressSearchComponent implements OnInit {
 	ngOnInit() {
 		this.location.marker.draggable = true;
 	}
-updateOnMap() {
-    let full_address:string = this.location.address_level_1 || ""
-    if (this.location.address_level_2) full_address = full_address + " " + this.location.address_level_2
-    if (this.location.address_state) full_address = full_address + " " + this.location.address_state
-    if (this.location.address_country) full_address = full_address + " " + this.location.address_country
- 
-    this.findLocation(full_address);
-  }
-  findLocation(address) {
-    if (!this.geocoder) this.geocoder = new google.maps.Geocoder()
-    this.geocoder.geocode({
-      'address': address
-    }, (results, status) => {
-      console.log(results);
-      if (status == google.maps.GeocoderStatus.OK) {
-                // decompose the result
-      } else {
-        alert("Sorry, this search produced no results.");
-      }
-    })
-  }
-  markerDragEnd(m: any, $event: any) {
-   this.location.marker.lat = m.coords.lat;
-   this.location.marker.lng = m.coords.lng;
-   this.findAddressByCoordinates();
-  }
+	updateOnMap() {
+		let full_address: string = this.location.address_level_1 || ""
+		if (this.location.address_level_2) full_address = full_address + " " + this.location.address_level_2
+		if (this.location.address_state) full_address = full_address + " " + this.location.address_state
+		if (this.location.address_country) full_address = full_address + " " + this.location.address_country
 
-  findAddressByCoordinates() {
-    this.geocoder.geocode({
-      'location': {
-        lat: this.location.marker.lat,
-        lng: this.location.marker.lng
-      }
-    }, (results, status) => {
-      this.decomposeAddressComponents(results);
-    })
-  }
+		this.findLocation(full_address);
+	}
+	findLocation(address) {
+		if (!this.geocoder) this.geocoder = new google.maps.Geocoder()
+		this.geocoder.geocode({
+			'address': address
+		}, (results, status) => {
+			if (status == google.maps.GeocoderStatus.OK) {
+				window.alert(results[0].geometry.location.lat())
+				this.location.lat = results[0].geometry.location.lat();
+				this.location.lng = results[0].geometry.location.lng();
+				this.location.marker.lat = results[0].geometry.location.lat();
+				this.location.marker.lng = results[0].geometry.location.lng();
 
-  decomposeAddressComponents(addressArray) {
-    if (addressArray.length == 0) return false;
-    let address = addressArray[0].address_components;
- 
-    for(let element of address) {
-      if (element.length == 0 && !element['types']) continue
- 
-      if (element['types'].indexOf('street_number') > -1) {
-        this.location.address_level_1 = element['long_name'];
-        continue;
-      }
-      if (element['types'].indexOf('route') > -1) {
-        this.location.address_level_1 += ', ' + element['long_name'];
-        continue;
-      }
-      if (element['types'].indexOf('locality') > -1) {
-        this.location.address_level_2 = element['long_name'];
-        continue;
-      }
-      if (element['types'].indexOf('administrative_area_level_1') > -1) {
-        this.location.address_state = element['long_name'];
-        continue;
-      }
-      if (element['types'].indexOf('country') > -1) {
-        this.location.address_country = element['long_name'];
-        continue;
-      }
-      if (element['types'].indexOf('postal_code') > -1) {
-        this.location.address_zip = element['long_name'];
-        continue;
-      }
-    }
-  }
+			} else {
+				window.alert("Sorry, this search produced no results.");
+			}
+		})
+	}
+	markerDragEnd(m: any, $event: any) {
+		this.location.marker.lat = m.coords.lat;
+		this.location.marker.lng = m.coords.lng;
+		this.findAddressByCoordinates();
+	}
+
+	findAddressByCoordinates() {
+		this.geocoder.geocode({
+			'location': {
+				lat: this.location.marker.lat,
+				lng: this.location.marker.lng
+			}
+		}, (results, status) => {
+			this.decomposeAddressComponents(results);
+		})
+	}
+
+	decomposeAddressComponents(addressArray) {
+		if (addressArray.length == 0) return false;
+		let address = addressArray[0].address_components;
+
+		for (let element of address) {
+			if (element.length == 0 && !element['types']) continue
+
+			if (element['types'].indexOf('street_number') > -1) {
+				this.location.address_level_1 = element['long_name'];
+				continue;
+			}
+			if (element['types'].indexOf('route') > -1) {
+				this.location.address_level_1 += ', ' + element['long_name'];
+				continue;
+			}
+			if (element['types'].indexOf('locality') > -1) {
+				this.location.address_level_2 = element['long_name'];
+				continue;
+			}
+			if (element['types'].indexOf('administrative_area_level_1') > -1) {
+				this.location.address_state = element['long_name'];
+				continue;
+			}
+			if (element['types'].indexOf('country') > -1) {
+				this.location.address_country = element['long_name'];
+				continue;
+			}
+			if (element['types'].indexOf('postal_code') > -1) {
+				this.location.address_zip = element['long_name'];
+				continue;
+			}
+		}
+	}
 }
